3a8dc42e784570eeb50ee04f0b92d354133013ec
ALVIN <akkangara8@gmail.com>
2022-09-21 14:19:37 -0400

    covid-app

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBeDHIyG4d06BiP_ToSq0KpUkjphdZES3E",
+
+  authDomain: "covid--tracker-8a20d.firebaseapp.com",
+
+  projectId: "covid--tracker-8a20d",
+
+  storageBucket: "covid--tracker-8a20d.appspot.com",
+
+  messagingSenderId: "924739576802",
+
+  appId: "1:924739576802:web:f5635aa49ed0c06fa98fbb"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
